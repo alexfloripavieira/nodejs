fda5124d040f2fc352d1292c3996283e14b2b2da
Alexsander Vieira <alexsandervieira@hotmail.com>
2021-04-14 16:50:20 -0300

    nodejs

---
+++ fundamentos/variaveisEConstantes.js
@@ -0,0 +1,12 @@
+var a = 3
+console.log(a)
+
+let b = 4 //usar declaração de variavel usando let pois não dexa redeclarar com o mesmo nome dentro de um mesmo escopo
+console.log(b)
+
+a = 6
+b = 7
+console.log(a,b)
+
+const c = 5 // declaração de constante
+console.log(c)
